aed4faa7cada1fab7f27f11c9641f6b76e8f9e39
Paul Hwang <mothcar@gmail.com>
2017-02-14 15:22:48 +0900

    first time

---
+++ src/MountainBike.java
@@ -0,0 +1,25 @@
+/**
+ * Created by paul on 14/02/2017.
+ */
+public class MountainBike extends Bicycle {
+
+    // the MountainBike subclass adds one field
+    public int seatHeight;
+
+    // the MountainBike subclass has one constructor
+    public MountainBike(int startHeight, int startCadence, int startSpeed, int startGear) {
+        super(startCadence, startSpeed, startGear);
+        seatHeight = startHeight;
+    }
+
+    // the MountainBike subclass adds one method
+    public void setHeight(int newValue) {
+        seatHeight = newValue;
+    }
+
+    public void setCadence(int newValue) {
+        seatHeight = newValue;
+
+    }
+
+}
